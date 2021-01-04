f8ca37db17281ac560aa398738dac9c080c0ded7
mfykmn <mfykmn@gmail.com>
2021-01-04 18:13:01 +0900

    Fixed annotation

--- payment-service/src/main/kotlin/com/example/sagas/PaymentServiceMain.kt
+++ payment-service/src/main/kotlin/com/example/sagas/PaymentServiceMain.kt
@@ -6,13 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Import
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
 @Configuration
 @Import(TramMessageProducerJdbcConfiguration::class,
     EventuateTramKafkaMessageConsumerConfiguration::class)
-@ComponentScan
 class PaymentServiceMain
 
 fun main(args: Array<String>) {
