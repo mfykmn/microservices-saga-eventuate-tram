3ece1b47652332e785836b385416266418e0c6dd
mfykmn <mfykmn@gmail.com>
2021-01-09 23:28:44 +0900

    Fixed Kotlin way

--- payment-service/src/main/kotlin/com/example/sagas/sagas/createorder/ReserveInvoiceCommand.kt
+++ payment-service/src/main/kotlin/com/example/sagas/sagas/createorder/ReserveInvoiceCommand.kt
@@ -3,10 +3,4 @@ package com.example.sagas.sagas.createorder
 import com.example.sagas.domain.entity.OrderId
 import io.eventuate.tram.commands.common.Command
 
-class ReserveInvoiceCommand : Command {
-    var orderId: OrderId
-
-    constructor(orderId: OrderId) {
-        this.orderId = orderId
-    }
-}
+data class ReserveInvoiceCommand(val orderId: OrderId) : Command
